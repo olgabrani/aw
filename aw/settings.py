3fe1a0294d617c40b205899950238d85798d1edf
Olga Brani <olgabrani78@gmail.com>
2015-12-25 17:28:13 +0200

    Use local_settings

--- aw/settings.py
+++ aw/settings.py
@@ -135,6 +135,11 @@ WORDS = {
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'athenswarriorcontact@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'my-email@gmail.com'
+EMAIL_HOST_PASSWORD = 'my-password'
 EMAIL_PORT = 587
+
+try:
+    from local_settings import *
+except ImportError as e:
+    pass
